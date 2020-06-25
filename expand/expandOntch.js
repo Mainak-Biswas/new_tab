b41269593196ad5852c69d19a9c73aabfc5ebbd2
Mainak Biswas <38928668+Mainak-Biswas@users.noreply.github.com>
2020-06-25 10:20:44 +0530

    expa nd mob

--- expand/expandOntch.js
+++ expand/expandOntch.js
@@ -17,7 +17,7 @@ function dragEl(elmnt)
     function dragTouchDown(e) 
     {
         e = e || window.event;
-    	e.preventDefault();
+        e.preventDefault();
         
         // get the mouse cursor position at startup:
         pos2 = e.touches[0].clientX;
@@ -35,12 +35,13 @@ function dragEl(elmnt)
     function elDrag(e)
     {
     	e = e || window.event;
-    	e.preventDefault();
         // calculate the new cursor position:
         pos1 = pos2 - e.touches[0].clientX;
         pos2 = e.touches[0].clientX;
         // set the element's new position:
         elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        
+        elmnt.style.transition = "left 0s";
       
         if(elmnt.offsetLeft <= -250)
         {
